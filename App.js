4ccfa3e89c566b90f51cd41a73872af77eccec9a
apple <apple@MacBook-Pro.local>
2022-12-02 10:50:18 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import RootApp from './RootApp';
+
+const App = () => {
+  return <RootApp />;
+};
+
+export default App;
